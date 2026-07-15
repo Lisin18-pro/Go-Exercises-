cfda9c086b33c7f5eb52c6f9f27e73736d479ab9
Lisin18-pro <lisinjoe058@gmail.com>
2026-07-14 23:39:51 -0700

    Fibonacci series.go

---
+++ Fibonacci series.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	fmt.Print("Enter the number of terms: ")
+	fmt.Scanln(&n)
+
+	a, b := 0, 1
+
+	fmt.Println("Fibonacci Series:")
+
+	for i := 0; i < n; i++ {
+		fmt.Print(a, " ")
+		a, b = b, a+b
+	}
+}
